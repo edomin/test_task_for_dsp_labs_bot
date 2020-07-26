2987560cd72d5b53385a5c1d920d1eefe45792a4
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-07-26 16:35:14 +0600

    fix silly errors

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import os
 import config
 import telebot
 
-home = return os.path.expanduser("~");
+home = os.path.expanduser("~");
 tokenfile = os.path.join(home, ".tgbots/test_task_for_dsp_labs_bot");
 with open(tokenfile) as file:
     token = file.read()
 
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
